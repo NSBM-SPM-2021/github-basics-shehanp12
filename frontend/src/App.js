f39831428a2a426fe161b9b819f7303331a65fa8
Roshan Piumal <55666479+shehanp12@users.noreply.github.com>
2021-08-17 01:08:11 +0530

    add aws serverlets

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import imageHero from './images/hero.png';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.state.loading = false;
+    this.state.votes = 0;
+    this.saveVote = this.saveVote.bind(this);
+  }
+
+  /**
+   * Component Did Mount
+   */
+
+  async componentDidMount() {}
+
+  /**
+   * Save Vote
+   */
+
+  async saveVote() {
+    this.setState({ votes: this.state.votes + 1 });
+  }
+
+  /**
+   * Render
+   */
+
+  render() {
+    return (
+      <div className="container">
+        <div className="hero">
+          <img src={imageHero} />
+        </div>
+
+        <div className="tagline">
+          a website built on serverless components via the serverless framework
+        </div>
+
+        <div className="buttonContainer">
+          <div
+            className={`button`}
+            onClick={() => {
+              this.saveVote();
+            }}
+          >
+            <div className={`buttonInner`}>
+              <div className={`buttonLeft`}>ß</div>
+              <div className="buttonRight">{this.state.votes}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
