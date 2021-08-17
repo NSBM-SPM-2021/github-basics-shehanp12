2dc8832093fba4b2486121047d51b0bb1b847bea
Roshan Piumal <55666479+shehanp12@users.noreply.github.com>
2021-08-18 00:04:29 +0530

    authentication

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,58 +1,17 @@
-import React, { Component } from 'react';
-import imageHero from './images/hero.png';
+import React, { useState, useEffect } from 'react';
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-    this.state.loading = false;
-    this.state.votes = 0;
-    this.saveVote = this.saveVote.bind(this);
-  }
+const App = () => {
 
-  /**
-   * Component Did Mount
-   */
+  
+  const [isAuthenticating, setIsAuthenticating] = useState(true);
+  const [isAuthenticated, userHasAuthenticated] = useState(false);
 
-  async componentDidMount() {}
+  return (
+    <div>
+      Scratch
+    </div>
+  )
 
-  /**
-   * Save Vote
-   */
-
-  async saveVote() {
-    this.setState({ votes: this.state.votes + 1 });
-  }
-
-  /**
-   * Render
-   */
-
-  render() {
-    return (
-      <div className="container">
-        <div className="hero">
-          <img src={imageHero} />
-        </div>
-
-        <div className="tagline">
-          a website built on serverless components via the serverless framework
-        </div>
-
-        <div className="buttonContainer">
-          <div
-            className={`button`}
-            onClick={() => {
-              this.saveVote();
-            }}
-          >
-            <div className={`buttonInner`}>
-              <div className={`buttonLeft`}>ß</div>
-              <div className="buttonRight">{this.state.votes}</div>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
 }
+
+export default App;
