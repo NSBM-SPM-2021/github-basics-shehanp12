0c0557bbdff3d294a8839e3a5fdaf58dc79193f7
Roshan Piumal <55666479+shehanp12@users.noreply.github.com>
2021-08-22 15:15:34 +0530

    configs in aws

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,3 +11,29 @@ ReactDOM.render((
 ), document.getElementById('root'));
 
 serviceWorker.unregister();
+
+
+
+Amplify.configure({
+  Auth: {
+    mandatorySignIn: true,
+    region: config.cognito.REGION,
+    userPoolId: config.cognito.USER_POOL_ID,
+    identityPoolId: config.cognito.IDENTITY_POOL_ID,
+    userPoolWebClientId: config.cognito.APP_CLIENT_ID
+  },
+  Storage: {
+    region: config.s3.REGION,
+    bucket: config.s3.BUCKET,
+    identityPoolId: config.cognito.IDENTITY_POOL_ID
+  },
+  API: {
+    endpoints: [
+      {
+        name: "prod-notes-api",
+        endpoint: config.apiGateway.URL,
+        region: config.apiGateway.REGION
+      },
+    ]
+  }
+});
